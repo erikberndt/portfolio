77456ef025326205fa1f3d7d6079cbdc4a15f129
erikberndt <erikberndt@gmail.com>
2016-03-01 21:08:53 +0100

    Erster Commit

---
+++ sidebar-contact.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ * Seiteleiste: Kontakt
+ *
+ */
+?>
+
+<div class="col col-12 px1 sm-px2 mt1 mb3 sm-mt3">
+	<h2 class="main-blog__title">Kontakt</h2>
+	<hr class="mb3 mt1">
+	<p>Sollten Sie Fragen haben, zögern Sie nicht Kontakt zu uns aufzunehmen.</p>
+	<div>
+		<h2 class="mt2 sidebar__heading inline-block">Sekretariat</h2>
+		<hr class="mb2">
+		<div class="clearfix">
+			<div class="col col-12">
+				<div class="flex flex-start  ">
+					Vanessa Scott
+					<div class="address__icon mr2 b1"></div>
+					<div>
+						<span class="block">Petermannstr. 35</span>
+						<span class="block">30459 Hannover</span>
+					</div>
+				</div>
+			</div>
+			<div class="col col-12">
+				<div class="border-top sidebar__separator mt2 mb2"></div>
+			</div>
+			<div class="col col-12">
+				<div class="flex flex-center  ">
+					<div class="phone__icon b1 mr2"></div>
+					<span class="block">0511 2344007</span>
+				</div>
+				<div class="flex flex-center  ">
+					<div class="mail__icon mr2 b1"></div>
+					<span class="block">Mail schreiben</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+</div>
